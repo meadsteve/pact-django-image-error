6c4051f10f53f24c1e71413347104b2711b21c6c
Steve Brazier <steveb@spotify.com>
2018-12-04 08:57:21 +0100

    Empty django 1.11 & pact image

---
+++ bug/simple_test/tests.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+
+class ReplicationTestCase(TestCase):
+
+    def test_the_truth(self):
+        self.assertTrue(True)
